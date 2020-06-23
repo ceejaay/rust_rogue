236d43ee567d02421060ccd59f3bf4090ce194b7
Chad Jemmett <cjemmett@wehco.com>
2020-06-23 15:08:26 -0500

    pulled the readme

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tcod::colors::*;
 use tcod::console::*;
+use std::cmp;
+use rand::Rng;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -107,9 +109,18 @@ fn create_room(room: Rect, map: &mut Map) {
     }
 }
 
-//create h tunnel h ere
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty()
+    }
+}
 
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
 
+}
 
 
 
@@ -120,6 +131,7 @@ fn make_map() -> Map {
     let room2 = Rect::new(50, 15, 10, 15);
     create_room(room1, &mut map);
     create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
 
     // map[30][22] = Tile::wall();
     // map[50][22] = Tile::wall();
