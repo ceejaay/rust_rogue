68f7b72fec112efdb8ab91c0016ff18c83b34934
Chad Jemmett <cjemmett@wehco.com>
2020-06-23 13:06:35 -0500

    can walk around a room

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use tcod::console::*;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
+
 const LIMIT_FPS: i32 = 20;
 
 const MAP_WIDTH: i32  = 80;
@@ -32,7 +33,7 @@ struct Game {
 
 struct Tile {
     blocked: bool,
-    block_sight: bool,
+    blocked_sight: bool,
 
 }
 
@@ -40,7 +41,7 @@ impl Tile {
     pub fn empty() -> Self {
         Tile {
             blocked: false,
-            block_sight: false,
+            blocked_sight: false,
         }
     }
 
@@ -51,6 +52,25 @@ impl Tile {
         }
     }
 }
+
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Object {
     x: i32,
@@ -64,9 +84,13 @@ impl Object {
         Object {x, y, char, color}
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
+        // self.x += dx;
+        // self.y += dy;
     }
 
     pub fn draw(&self, con: &mut dyn Console) {
@@ -75,41 +99,93 @@ impl Object {
     }
 }
 
-    fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
-        // you can import just into a function, cool
-        use tcod::input::Key;
-        use tcod::input::KeyCode::*;
-
-        let key = tcod.root.wait_for_keypress(true);
-        match key {
-            Key {
-                code: Enter,
-                alt: true,
-                ..
-            } => {
-                let fullscreen = tcod.root.is_fullscreen();
-                tcod.root.set_fullscreen(!fullscreen);
-            }
-            Key {code: Escape, ..} => return true,
-            //
-            // movement keys
-            Key { code: Down, .. } => player.move_by(0, -1),
-            Key { code: Up, .. } => player.move_by(0, 1),
-            Key { code: Left, .. } => player.move_by(-1, 0),
-            Key { code: Right, .. } => player.move_by(1, 0),
-
-            _ => {}
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
         }
-        false
     }
+}
+
+//create h tunnel h ere
+
+
 
 
 
 fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize] MAP_WIDTH as usize];
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    // to pillars to test the map
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+
+    // map[30][22] = Tile::wall();
+    // map[50][22] = Tile::wall();
     map
 }
 
+
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].blocked_sight;
+            if wall {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+
+        }
+    }
+
+    for object in objects {
+        object.draw(&mut tcod.con)
+    }
+
+        blit(
+            &tcod.con,
+            (0, 0),
+            (MAP_WIDTH, MAP_HEIGHT ),
+            &mut tcod.root,
+            (0, 0),
+            1.0,
+            1.0,
+            );
+}
+
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
+    // you can import just into a function, cool
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key {code: Escape, ..} => return true,
+        //
+        // movement keys
+        Key { code: Down, .. } => player.move_by(0, -1, game),
+        Key { code: Up, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
+
+        _ => {}
+    }
+    false
+}
 
 fn main() {
     tcod::system::set_fps(LIMIT_FPS);
@@ -125,18 +201,23 @@ fn main() {
         .title("Rust/libtcod tutorial")
         .init();
 
-    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     let mut tcod = Tcod { root, con };
 
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+    let player = Object::new(25, 23, '@', WHITE);
 
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+
+    let game = Game {
+        map: make_map(),
+    };
 
     // here's where all the objects will go into the array
     let mut objects = [player, npc];
 
     // Main game loop.
+    //
     while !tcod.root.window_closed() {
         tcod.con.clear();
 
@@ -146,21 +227,14 @@ fn main() {
             object.draw(&mut tcod.con)
         }
         // not sure what blit is?
-        blit(
-            &tcod.con,
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root,
-            (0, 0),
-            1.0,
-            1.0,
-            );
         //
         // tcod.con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        render_all(&mut tcod, &game, &objects);
+
         tcod.root.flush();
         // tcod.root.wait_for_keypress(true);
         let player = &mut objects[0];
-        let exit = handle_keys(&mut tcod, player);
+        let exit = handle_keys(&mut tcod, &game, player);
         if exit {
             break;
         }
